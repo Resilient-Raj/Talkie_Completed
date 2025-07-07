d2206db89dbffe0b8b5d7cc3d3ea78bf88005f1b
Raj sharma <raj26sharma10.2004@gmail.com>
2025-07-08 03:22:40 +0530

    Update useGetOtherUsers.jsx

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -5,23 +5,22 @@ import { setOtherUsers } from '../redux/userSlice';
 import { BASE_URL } from '../index';
 
 const useGetOtherUsers = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchOtherUsers = async () => {
-            try {
-                axios.defaults.withCredentials = true;
-                const res = await axios.get(`${BASE_URL}/api/v1/user`);
-                // store
-                console.log("other users -> ",res);
-                dispatch(setOtherUsers(res.data));
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchOtherUsers();
-    }, [])
+  useEffect(() => {
+    const fetchOtherUsers = async () => {
+      try {
+        const res = await axios.get(${BASE_URL}/api/v1/user, {
+          withCredentials: true
+        });
+        console.log("other users ->", res);
+        dispatch(setOtherUsers(res.data));
+      } catch (error) {
+        console.log("error ->", error);
+      }
+    };
+    fetchOtherUsers();
+  }, [dispatch]);
+};
 
-}
-
-export default useGetOtherUsers
+export default useGetOtherUsers;
