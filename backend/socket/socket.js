80bcc152e85c6a15024ec0fa3bad72422446fc8a
Raj sharma <raj26sharma10.2004@gmail.com>
2025-07-08 15:08:11 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:'https://talkie-completed-6.onrender.com',
+        origin:'https://talkie-completed-fe.onrender.com',
         methods:['GET', 'POST'],
     },
 });
